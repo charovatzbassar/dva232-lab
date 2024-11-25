902d9b6d20b0e5882979fecca3d187683524916f
Basar Carovac <carovac.basar@gmail.com>
2024-11-25 16:32:26 +0100

    Add modal

---
+++ app/src/main/java/com/example/dva232/view/composables/ModalDialog.kt
@@ -0,0 +1,56 @@
+package com.example.dva232.view.composables
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
+
+@Composable
+fun ModalDialog(isDialogOpen: Boolean, onDismissRequest: () -> Unit, text: String) {
+    if (isDialogOpen) {
+        Dialog(
+            onDismissRequest = { onDismissRequest() },
+            properties = DialogProperties(dismissOnClickOutside = true)
+        ) {
+            Box(
+                modifier = Modifier
+                    .size(300.dp, 200.dp)
+                    .background(Color.White, RoundedCornerShape(16.dp))
+                    .padding(16.dp)
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.SpaceBetween,
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    Text(text = text, style = MaterialTheme.typography.titleMedium)
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Button(onClick = { onDismissRequest() }) {
+                        Text("Close")
+                    }
+                }
+            }
+        }
+    }
+
+}
--- app/src/main/java/com/example/dva232/view/pages/ConvertPage.kt
+++ app/src/main/java/com/example/dva232/view/pages/ConvertPage.kt
@@ -1,5 +1,6 @@
 package com.example.dva232.view.pages
 
+import android.content.res.Configuration
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -14,70 +15,113 @@ import androidx.compose.runtime.mutableDoubleStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.dva232.view.composables.DropdownInput
+import com.example.dva232.view.composables.ModalDialog
 import com.example.dva232.view.composables.Result
 import com.example.dva232.view.util.Currency
 
 @Composable
 fun ConvertPage(currencies: List<Currency>, navController: NavController) {
     var result by remember { mutableDoubleStateOf(0.0) }
+    var isResultInvalid by remember { mutableStateOf(false) }
 
     var amount by remember { mutableStateOf("") }
     val selectedFromCurrency = remember { mutableStateOf("EUR") }
     val selectedToCurrency = remember { mutableStateOf("SEK") }
 
     val options = currencies.map { c -> c.code }
-
     Column(
         modifier = Modifier.padding(16.dp)
     ) {
+        when (LocalConfiguration.current.orientation) {
+            Configuration.ORIENTATION_PORTRAIT -> {
+                Row(
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    DropdownInput(
+                        text = "From",
+                        options = options,
+                        selectedOption = selectedFromCurrency,
+                        modifier = Modifier.weight(1f)
+                    )
 
-        Row(
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            DropdownInput(
-                text = "From",
-                options = options,
-                selectedOption = selectedFromCurrency,
-                modifier = Modifier.weight(1f)
-            )
+                    DropdownInput(
+                        text = "To",
+                        options = options,
+                        selectedOption = selectedToCurrency,
+                        modifier = Modifier.weight(1f)
+                    )
+                }
 
-            DropdownInput(
-                text = "To",
-                options = options,
-                selectedOption = selectedToCurrency,
-                modifier = Modifier.weight(1f)
-            )
+                OutlinedTextField(
+                    value = amount,
+                    onValueChange = { amount = it },
+                    label = { Text("Amount") },
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 16.dp)
+                )
+
+            }
+
+            else -> {
+                Row(
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    DropdownInput(
+                        text = "From",
+                        options = options,
+                        selectedOption = selectedFromCurrency
+                    )
+
+                    DropdownInput(
+                        text = "To",
+                        options = options,
+                        selectedOption = selectedToCurrency
+                    )
+
+
+                    OutlinedTextField(
+                        value = amount,
+                        onValueChange = { amount = it },
+                        label = { Text("Amount") },
+                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                    )
+                }
+            }
         }
-
-        OutlinedTextField(
-            value = amount,
-            onValueChange = { amount = it },
-            label = { Text("Amount") },
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(top = 16.dp)
-        )
 
         Button(
             onClick = {
-                val fromCurrency = currencies.find { c -> c.code == selectedFromCurrency.value }
-                val toCurrency = currencies.find { c -> c.code == selectedToCurrency.value }
+                if (amount == "" || amount.toDouble() <= 0f) {
+                    isResultInvalid = true
+                    return@Button
+                } else {
+                    val fromCurrency =
+                        currencies.find { c -> c.code == selectedFromCurrency.value }
+                    val toCurrency = currencies.find { c -> c.code == selectedToCurrency.value }
 
-                result = (toCurrency!!.rate * amount.toDouble()) / fromCurrency!!.rate
+                    result = (toCurrency!!.rate * amount.toDouble()) / fromCurrency!!.rate
+                }
             },
             modifier = Modifier.padding(top = 16.dp).fillMaxWidth()
         ) {
             Text("Submit")
         }
 
-
         Result(resultValue = result)
+
+        ModalDialog(text = "This amount is invalid", isDialogOpen = isResultInvalid, onDismissRequest = {
+            isResultInvalid = false
+        })
+
     }
 }
